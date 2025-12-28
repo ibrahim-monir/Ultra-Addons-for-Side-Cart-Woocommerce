fd2bfcf1d13930adf927fc41af81904531e5604f
Ibrahim Monir <54437935+ibrahim-monir@users.noreply.github.com>
2025-12-28 18:10:41 +0600

    Add plugin header information for WooCommerce addon

--- ultra-addons-for-side-cart-woocommerce.php
+++ ultra-addons-for-side-cart-woocommerce.php
@@ -1,1 +1,17 @@
+<?php
+/**
+ * Plugin Name: Ultra Addons for Side Cart Woocommerce
+ * Plugin URI: #
+ * Description: 10+ Essential Addons for Side Cart Woocommerce.
+ * Version: 1.0.0
+ * Author: Ibrahim Monir
+ * Author URI: #
+ * License: GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain: ultimate-addons-scwoo
+ * Domain Path: /languages
+ */
 
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
